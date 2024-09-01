ccde01dcad6d35d68713ec9e62f3527e8bfbe9c0
ein <168879033+einleft@users.noreply.github.com>
2024-09-01 01:08:31 +0100

    Update dataTypes.js

--- dataTypes/dataTypes.js
+++ dataTypes/dataTypes.js
@@ -27,5 +27,38 @@ console.log('i am a string' / 2); // NaN, not a number this would be erroneous
 console.log(NaN + 1); // NaN
 console.log('im a string' / 2 - 1); // NaN
 
-// BIGINT TODO
-// 2^53-1 = 9007199254740991
+// the number data type cannot represent
+// an integer value larger than (2^53-1) (that's 9007199254740991) or less than
+// (-2^53-1)
+
+// two numbers right above the safe range are the same
+console.log(9007199254740991 + 1);
+console.log(9007199254740991 + 2); // this doesn't go above the +1
+
+// creating a bigInt
+const bigInt = 1234567890123456789012345678901234567890n; // the 'n' at the end represents the bigInt
+
+// String
+let str = "hello";
+let str2 = 'single quote';
+let phrase = `can embed another ${str}` // using backticks
+
+// Embedded variables
+let name = 'john';
+console.log(`hello, ${name}`);
+console.log(`the result is: ${1 + 2}`);
+console.log("the result is: ${1 + 2}"); // cannot embed the expression
+
+// Booleans
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+// booleans are only to check true or false
+
+// boolean values also come as a result of comparisons
+let isGreater = 4 > 1;
+console.log(isGreater); // 4 is greater than 1 true
+
+isGreater = 4 > 6
+console.log(isGreater); // false, 4 is not greater than 6
+
+// NULL VALUES TODO
